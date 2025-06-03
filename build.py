1ed4772b71add2de78d3ae4a272a5bf0f954e38e
HilltopWorks <115959038+HilltopWorks@users.noreply.github.com>
2025-06-02 18:51:29 -0700

    Add files via upload

--- build.py
+++ build.py
@@ -34,6 +34,6 @@ def build():
     FMV_PATHS = [r"FMV/OPEN_EDIT.avi", r"FMV/ENDING_EDIT.avi", r"FMV/COMP_EDIT.avi"]
     INDICES = [47, 42, 39]
 
-    #VideoHill.replaceAllPS1Video(r"mkpsxiso/JC.bin", FMV_PATHS, INDICES)
+    VideoHill.replaceAllPS1Video(r"mkpsxiso/JC.bin", FMV_PATHS, INDICES)
 
 build()
